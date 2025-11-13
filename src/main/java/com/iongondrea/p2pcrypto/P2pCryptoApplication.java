19ea3c76c6618866a49429dbdf8d0e234c5281a0
Ion Gondrea <gondrea.02@gmail.com>
2025-11-13 15:58:09 +0200

    Add structure

---
+++ src/main/java/com/iongondrea/p2pcrypto/P2pCryptoApplication.java
@@ -0,0 +1,12 @@
+package com.iongondrea.p2pcrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pCryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2pCryptoApplication.class, args);
+    }
+}
